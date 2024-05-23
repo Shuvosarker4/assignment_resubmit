dbff7c017822d4a0c94a98ba94da8d77b2e8efc2
Shuvosarker4 <shuvo01797451@gmail.com>
2024-05-23 17:43:39 +0600

    order interface and model created

---
+++ src/modules/Order/Order.model.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+import { TOrder } from "./Order.Interface";
+
+const orderSchema = new Schema<TOrder>({
+  email: {
+    type: String,
+    required: true,
+  },
+  productId: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+});
+
+export const Order = model<TOrder>("Order", orderSchema);
