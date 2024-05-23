4f1e0c4a0b976bea3b3c2e278c3285e3d97d620a
Shuvosarker4 <shuvo01797451@gmail.com>
2024-05-23 17:28:08 +0600

    dotenv file added

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import app from "./app";
 import mongoose from "mongoose";
+import config from "./config";
 
 async function main() {
-  await mongoose.connect(
-    "mongodb+srv://product_recap:iaFP6HJpxQISwUT3@cluster0.zjkcwvl.mongodb.net/Product_Recap?retryWrites=true&w=majority&appName=Cluster0"
-  );
-  const port = 5000;
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  await mongoose.connect(config.db_url as string);
+
+  app.listen(config.port, () => {
+    console.log(`Server is running on port ${config.port}`);
   });
 }
 
